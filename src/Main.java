a8b1113bf7e54c294485c2fa5a48615e20554681
ZulemaButler <zxbutl3r@gmail.com>
2023-08-02 21:07:27 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is is a new code");
-
+        System.out.println("this is a new code");
+        System.out.println("Hola");
     }
 }
